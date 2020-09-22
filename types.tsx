8e239c91cef2aeb2012570eac96168989503594f
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-09-22 15:01:36 -0400

    convert to new repo history

---
+++ types.tsx
@@ -0,0 +1,27 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Calendar: undefined;
+  Edit: undefined;
+  Stats: undefined;
+  Settings: undefined;
+};
+
+export type CalendarParamList = {
+  CalendarScreen: undefined;
+};
+
+export type EditParamList = {
+  EditScreen: undefined;
+};
+
+export type StatsParamList = {
+  StatsScreen: undefined;
+};
+
+export type SettingsParamList = {
+  SettingsScreen: undefined;
+};
