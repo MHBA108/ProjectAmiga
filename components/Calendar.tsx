fb61bb33c43942283dc12363df6589b7ad1c8a41
Alex Welsh <welsh6263@gmail.com>
2020-09-23 12:51:40 -0400

    initial merge of my clock and calendar. onDateChange has error

---
+++ components/Calendar.tsx
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+import {View, Text, StyleSheet} from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
+import moment from "moment";
+
+export default class Calendar extends Component <{}, {selectedStartDate: Date | null}>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+          selectedStartDate: null,
+        };
+        this.onDateChange = this.onDateChange.bind(this);
+      }
+
+    onDateChange(date: Date) {
+    this.setState({
+        selectedStartDate: date,
+    });
+
+    }
+
+    render() {
+        const { selectedStartDate } = this.state;
+        const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+        return (
+          <View style={styles.container}>
+            <CalendarPicker
+              onDateChange={this.onDateChange}
+              textStyle={{fontFamily: 'HindSiliguri_400Regular', color: '#464D77'}}
+            />
+            <View>
+              <Text style={styles.textStyle}>SELECTED DATE:{ startDate }</Text>
+            </View>
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFFFFF',
+      marginTop: 25,
+      fontFamily: 'HindSiliguri_400Regular',
+      color: 'red'
+    },
+
+    textStyle: {
+        fontFamily: 'HindSiliguri_400Regular',
+        color: 'red'
+      },
+  });
+
+
--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,16 +1,44 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import Clock from "../components/Clock";
+import Calendar from "../components/Calendar";
+import Slider from '@react-native-community/slider';
 
 export default function CalendarScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Calendar</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/CalendarScreen.tsx" />
-    </View>
+    <ScrollView>
+      <Text style={styles.todayStyle}>Today</Text>
+      <Clock/>
+      <View style={{ 
+          backgroundColor: "#6699CC", 
+          padding: "10, 10, 10, 10",
+          borderRadius:10,
+          marginLeft: 10, 
+          marginRight: 10,
+          }}>
+        <Text style={styles.questionStyle}>How are you feeling today?</Text>
+        <Slider
+          style={{
+            height: 40,
+            marginLeft: 30,
+            marginRight: 30
+          }}
+          minimumValue={0}
+          maximumValue={4}
+          minimumTrackTintColor="#464D77"
+          maximumTrackTintColor="white"
+          step={1}
+        />
+        <View style={styles.textCon}>
+                    <Text style={styles.textStyle}>Terrible</Text>
+                    <Text style={styles.textStyle}>Neutral</Text>
+                    <Text style={styles.textStyle}>Great</Text>
+                </View>
+      </View>
+      <Calendar/>
+    </ScrollView>
   );
 }
 
@@ -29,4 +57,34 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+
+  textCon: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  todayStyle:{
+    color: '#464D77',
+    fontSize: 75,
+    fontWeight: 'bold',
+    fontFamily: 'HindSiliguri_700Bold',
+    paddingTop: 20,
+    marginLeft: 10
+  },
+
+  textStyle:{
+    color: 'white',
+    fontSize: 20,
+    marginLeft: 10
+  },
+
+  questionStyle:{
+    color: 'white',
+    fontSize: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    textAlign: 'center'
+  }
 });
