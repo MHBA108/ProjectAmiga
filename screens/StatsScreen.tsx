52e7edd969b5533c211aca6ceafa93c3f0191260
Alex Welsh <welsh6263@gmail.com>
2020-10-12 18:43:30 -0400

    added graph

--- screens/StatsScreen.tsx
+++ screens/StatsScreen.tsx
@@ -8,6 +8,7 @@ import {
   Alert,
 } from "react-native";
 import { Avatar } from "react-native-elements";
+import { LineChart } from "react-native-chart-kit";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
@@ -55,53 +56,98 @@ export default function StatsScreen() {
               <Text style={{ color: "#464D77" }}> Longest Log Streak: 23</Text>
             </View>
           </View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              width: windowWidth * 0.95,
-              backgroundColor: "#464D77",
-              paddingHorizontal: 15,
-              paddingVertical: 10,
-              margin: 10,
-              borderRadius: 10,
-            }}
-          >
-            <View style={{ backgroundColor: "#464D77" }}>
-              <Text
-                style={{
-                  color: "white",
-                  backgroundColor: "#464D77",
-                  fontSize: 25,
-                }}
+          <View style={styles.graphContainer}>
+            <View style={styles.graphHeaderStyle}>
+              <View style={{ backgroundColor: "#464D77" }}>
+                <Text
+                  style={{
+                    color: "white",
+                    backgroundColor: "#464D77",
+                    fontSize: 25,
+                  }}
+                >
+                  Mood Chart
+                </Text>
+              </View>
+              <View
+                style={{ flexDirection: "row", borderRadius: 10, padding: 3 }}
               >
-                Mood Chart
-              </Text>
+                <TouchableHighlight
+                  underlayColor="none"
+                  onPress={() => Alert.alert("Week pressed")}
+                  style={styles.graphRangeStyle}
+                >
+                  <Text style={{ color: "white" }}>W</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  underlayColor="none"
+                  onPress={() => Alert.alert("Month pressed")}
+                  style={styles.graphRangeStyle}
+                >
+                  <Text style={{ color: "white" }}>M</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  underlayColor="none"
+                  onPress={() => Alert.alert("Year pressed")}
+                  style={styles.graphRangeStyle}
+                >
+                  <Text style={{ color: "white" }}>Y</Text>
+                </TouchableHighlight>
+              </View>
             </View>
-            <View
-              style={{ flexDirection: "row", borderRadius: 10, padding: 3 }}
-            >
-              <TouchableHighlight
-                underlayColor="none"
-                onPress={() => Alert.alert("Week pressed")}
-                style={styles.graphRangeStyle}
-              >
-                <Text style={{ color: "white" }}>W</Text>
-              </TouchableHighlight>
-              <TouchableHighlight
-                underlayColor="none"
-                onPress={() => Alert.alert("Month pressed")}
-                style={styles.graphRangeStyle}
-              >
-                <Text style={{ color: "white" }}>M</Text>
-              </TouchableHighlight>
-              <TouchableHighlight
-                underlayColor="none"
-                onPress={() => Alert.alert("Year pressed")}
-                style={styles.graphRangeStyle}
-              >
-                <Text style={{ color: "white" }}>Y</Text>
-              </TouchableHighlight>
+            <View style={{justifyContent: "center", alignItems: "center"}}>
+              <Text>Bezier Line Chart</Text>
+              <LineChart
+                data={{
+                  labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                  ],
+                  datasets: [
+                    {
+                      data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                      ],
+                    },
+                  ],
+                }}
+                width={Dimensions.get("window").width * .85} // from react-native
+                height={220}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#e26a00",
+                  backgroundGradientFrom: "#fb8c00",
+                  backgroundGradientTo: "#ffa726",
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) =>
+                    `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726",
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                }}
+              />
             </View>
           </View>
         </SafeAreaView>
@@ -129,5 +175,26 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderRadius: 10,
     backgroundColor: "#464D77",
+  },
+
+  graphContainer: {
+    justifyContent: "space-between",
+    width: windowWidth * 0.95,
+    backgroundColor: "#464D77",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+
+  graphHeaderStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: windowWidth * 0.88,
+    backgroundColor: "#464D77",
+    // paddingHorizontal: 15,
+    // paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 10,
   },
 });
