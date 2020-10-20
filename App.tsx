49498aeb52624da8aed23487f3451f50da1396c3
Caroline <cjyoon3@emory.edu>
2020-10-19 21:56:08 -0400

    EStyleSheet global var

--- App.tsx
+++ App.tsx
@@ -16,10 +16,12 @@ import {
 import { AppLoading } from "expo";
 import * as Application from "expo-application";
 import * as firebase from "firebase";
+import EStyleSheet from "react-native-extended-stylesheet";
+import { Dimensions } from "react-native";
 
 // import { LogBox }​​​​​​​​​ from "react-native";
-LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
-LogBox.ignoreAllLogs();//Ignore all log notifications
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdmZhF_Qr8-5hLmVXtmq013CLnOpkG6Cc",
@@ -31,7 +33,6 @@ const firebaseConfig = {
     "159429589027-ftghqpo7042lo7miv41eucuvboc09mh6.apps.googleusercontent.com",
 };
 
-// so this 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
 console.log("application id: " + Application.applicationId);
@@ -39,9 +40,14 @@ console.log("application id: " + Application.applicationId);
 // TODO: Pressing back in the root tab crashes the app! I am not sure the arrow
 // should even be there.
 export default function App() {
+  const entireScreenWidth = Dimensions.get("window").width;
+  EStyleSheet.build({
+    $rem: entireScreenWidth / 380,
+  });
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  
+
   let [fontsLoaded] = useFonts({
     HindSiliguri_700Bold,
     HindSiliguri_400Regular,
