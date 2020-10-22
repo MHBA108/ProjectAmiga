637679e008073dd223c74aa5e6b658a9475fe69c
Alex Welsh <welsh6263@gmail.com>
2020-10-22 14:43:57 -0400

    started adding types for log backend

--- types.tsx
+++ types.tsx
@@ -29,8 +29,22 @@ export type UserProfileParamList = {
   UserProfileScreen: undefined;
 };
 
+// TODO: remove LogData in favor of Log
 export type LogData = {
   sliderValue: number;
   emotions: string[];
   color: string;
+};
+
+export type Log = {
+  moodPercentile: number;
+  text: string;
+  timestamp: Date;
+  moodWords: string[];
+};
+
+export type User = {
+  avatar: string;
+  streak: number;
+  // TODO: add achievments
 };
