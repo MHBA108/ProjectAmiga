b81b9a1a4e5a6f98298295ed851d09a51cb0dbab
MHBA108 <mariohba105@gmail.com>
2020-10-26 21:36:13 -0400

    Left aligned txt in TodoList

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -99,7 +99,10 @@ class TodoItem extends Component<{todo?: any, deleteTodo: Function, inCompleteTo
               onBlur={this.finishEditing}
               />
           ) : (
-            <Text style={[styles.text, isCompleted ? styles.strikeText : styles.unstrikeText]}>{textValue}</Text>
+            <Text style={[styles.text,
+                isCompleted ? styles.strikeText : styles.unstrikeText,
+                styles.input,
+              ]}>{textValue}</Text>
           )}
         </View>
         {isEditing ? (
@@ -138,6 +141,7 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
     marginBottom: 10,
+    textAlign: "left",
     color: COLORS.beige,
   },
   rowContainer: {
@@ -175,7 +179,7 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     fontSize: 18,
     fontFamily: "HindSiliguri_400Regular",
-    // marginVertical: 20,
+    marginVertical: 20,
     textAlign: "left",
     color: COLORS.beige,
   },
