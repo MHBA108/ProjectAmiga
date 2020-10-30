039115b8799ef7ad027c8506be224493c27f9a20
Julia <juleslevine13@gmail.com>
2020-10-30 14:04:34 -0400

    home

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,59 +1,78 @@
 import * as React from "react";
-import { ScrollView, SafeAreaView, Button } from "react-native";
+import {
+  ScrollView,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Platform,
+} from "react-native";
 import { AuthContext } from "../navigation/context";
 import { Text, View } from "../components/Themed";
 import Clock from "../components/Clock";
 import Calendar from "../components/Calendar";
 import CreateLog from "../components/CreateLog";
 import TodoList from "../components/TodoList";
-import firebase from "firebase";
+import firebase, { FirebaseError } from "firebase";
 import MyHeader from "../components/MyHeader";
 import { COLORS } from "../assets/COLORS";
-import { useNavigation } from "@react-navigation/native";
 
 import EStyleSheet from "react-native-extended-stylesheet";
 
+function renderGreeting(user: firebase.User | null) {
+  let timeOfDay = new Date(Date.now()).getHours();
+  let Greeting;
+  if (timeOfDay < 5) {
+    Greeting = "Good evening";
+  } else if (timeOfDay < 11) {
+    Greeting = "Good morning";
+  } else if (timeOfDay < 18) {
+    Greeting = "Good afternoon";
+  } else {
+    Greeting = "Good evening";
+  }
+
+  if (user == null) {
+    return Greeting + "!";
+  } else {
+    return Greeting + " " + user.displayName + "!";
+  }
+}
+
 const HomeScreen = (props: { navigation: any }) => {
   const [user, setUser] = React.useState(firebase.auth().currentUser);
-  const [value, onChangeText] = React.useState("Write note here ...");
   const authContext = React.useContext(AuthContext);
-  const navigation = useNavigation();
-
-  // TODO: reset streak if they didn't log the previous day
+  // TODO:
   React.useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user != null) {
-        console.log(user.displayName + " has logged in!");
+        console.log(user.displayName + " is logged in!");
         setUser(user);
       }
     });
   });
 
   return (
-    <SafeAreaView style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.container}>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={styles.scrollContainer}
+          >
+            <Text style={styles.textGreeting}>{renderGreeting(user)}</Text>
+            <Clock showDate={true} showTime={true} />
+            <View style={styles.spacer}></View>
+            <CreateLog sliderValue={50} noteText="" />
+            <Calendar />
+            <TodoList />
+          </ScrollView>
+        </View>
+      </TouchableWithoutFeedback>
       <MyHeader navigation={props.navigation} />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        style={styles.scrollContainer}
-      >
-        <Text style={styles.todayStyle}>
-          {"Welcome"} {user?.displayName}!
-        </Text>
-        <Clock showDate={true} showTime={true} />
-        <CreateLog sliderValue={50} noteText="" />
-        <Calendar />
-        <TodoList />
-        <Text style={{ alignContent: "stretch" }}>
-          {user == null ? "" : user.email}
-        </Text>
-        <Button
-          title="Log Off"
-          onPress={() => {
-            authContext.signOut();
-          }}
-        />
-      </ScrollView>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -66,50 +85,20 @@ const styles = EStyleSheet.create({
     justifyContent: "center",
     backgroundColor: COLORS.beige,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  scrollContainer: {
+    paddingHorizontal: "15rem",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-
-  textCon: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: COLORS.lightBlue,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-
-  note: {
-    height: 40,
-    color: "#CCD0E1",
-    marginLeft: 10,
-    marginRight: 10,
-  },
-
-  todayStyle: {
+  textGreeting: {
+    paddingTop: "25rem",
     color: COLORS.darkBlue,
-    fontSize: 34,
+    fontSize: "25rem",
     fontWeight: "bold",
     fontFamily: "HindSiliguri_700Bold",
-    marginLeft: 10,
+    textAlign: "right",
   },
-
-  textStyle: {
-    color: "white",
-    backgroundColor: COLORS.lightBlue,
-    fontSize: 20,
-  },
-
-  questionStyle: {
-    color: "white",
-    fontSize: 20,
-    marginLeft: 10,
-    marginRight: 10,
-    textAlign: "center",
+  spacer: {
+    width: "100%",
+    aspectRatio: 15 / 1,
+    backgroundColor: "transparent",
   },
 });
