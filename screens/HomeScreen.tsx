408917b7a8823b05284ea0ca27da7ba5463ca355
Caroline <cjyoon3@emory.edu>
2020-11-04 12:42:16 -0500

    try retrieving most recent log

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -13,7 +13,7 @@ import Clock from "../components/Clock";
 import Calendar from "../components/Calendar";
 import CreateLog from "../components/CreateLog";
 import TodoList from "../components/TodoList";
-import firebase, { FirebaseError } from "firebase";
+import firebase, { firestore } from "firebase";
 import MyHeader from "../components/MyHeader";
 import { COLORS } from "../assets/COLORS";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -40,13 +40,77 @@ function renderGreeting(user: firebase.User | null) {
     return Greeting + " " + user.displayName + "!";
   }
 }
+// TODO: Return type firestore.DocumentData of the most recent log
+// async function retrieveLastLog(user: firebase.User | null) {
+//   try {
+//     // Set State: Loading
+//     console.log("Retrieving Log");
+//     // Cloud Firestore: Query
+//     let initialQuery = await firestore()
+//       .collection("users")
+//       .doc(user?.uid)
+//       .collection("userLogs")
+//       .orderBy("timestamp", "desc")
+//       .limit(1);
+//     // Cloud Firestore: Query Snapshot
+//     let documentSnapshots = await initialQuery.get();
+//     // Cloud Firestore: Document Data
+//     let documentData = documentSnapshots.docs.map((document) =>
+//       document.data()
+//     );
+//     let logData = documentData[0];
+//     console.log(logData);
+//     console.log(logData.timestamp);
+//     return logData;
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
+// }
 
-function renderLogComp() {
+// function renderLogComp(user: firebase.User | null) {
+//   let hasLogged = false;
+//   let logData = retrieveLastLog(user);
+//   console.log("retrieved logData: " + logData);
+
+//   //TODO handle if no logs?
+//   if (logData === null) {
+//     return <CreateLog sliderValue={50} noteText="" />;
+//   }
+
+//   let mostRecentDay = new Date(logData.timestamp);
+
+//   let today = new Date(Date.now());
+//   let date = today.getDate();
+//   let month = today.getMonth();
+//   let year = today.getFullYear();
+
+//   if (mostRecentDay.getDate() == date) {
+//     if (mostRecentDay.getMonth() == month) {
+//       if (mostRecentDay.getFullYear() == year) {
+//         hasLogged = true;
+//       }
+//     }
+//   }
+
+//   if (hasLogged) {
+//     return (
+//       <TodayEntry
+//         moodPercentile={lastLog.moodPercentile}
+//         text={lastLog.text}
+//         timestamp={lastLog.timestamp}
+//         moodWords={lastLog.moodWords}
+//       />
+//     );
+//   } else {
+//     return <CreateLog sliderValue={50} noteText="" />;
+//   }
+// }
+
+// TODO: delete this function in favor of the commented out one above
+function renderLogComp(user: firebase.User | null) {
   let hasLogged = false;
-
-  // TODO: Retrieve most recent log and check if today
-  // let lastLog = ??? .collection("userLogs").orderBy("timestamp", "desc")
-  let dummyTimestamp = "2020-11-03T23:52:59-04:00";
+  let dummyTimestamp = "2020-11-04T23:52:59-04:00";
   let lastLog = new Date(dummyTimestamp);
 
   let today = new Date(Date.now());
@@ -103,7 +167,7 @@ const HomeScreen = (props: { navigation: any }) => {
             <Text style={styles.textGreeting}>{renderGreeting(user)}</Text>
             <Clock showDate={true} showTime={true} />
             <View style={styles.spacer}></View>
-            {renderLogComp()}
+            {renderLogComp(user)}
             <Calendar />
             <TodoList />
           </ScrollView>
